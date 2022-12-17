bff3f5e68aacde0d1f74ceda877283028e76cfdb
SabaShahdin <sabashahdin20@gmail.com>
2022-12-17 21:34:24 +0500

    pf lab2

---
+++ task10.cpp
@@ -0,0 +1,12 @@
+# include <iostream>
+using namespace std;
+main ()
+{
+ cout <<"   ------------------------------          "<<endl;
+ cout <<"      o      ^  ^                         "<<endl;
+ cout <<"        o    (oo)\\____________           "<<endl;
+ cout <<"                                            "<<endl;
+ cout <<"             (  )\\           )\\/\\      "<<endl;
+ cout <<"                     ||-----w   |            "<<endl;
+ cout <<"                     ||        ||            "<<endl;
+ }
---
+++ task5.cpp
@@ -0,0 +1,21 @@
+# include <iostream>
+using namespace std;
+main ()
+{
+ cout <<"         ############################################################################### "<<endl;
+ cout <<"         ##                                                                           ##  "<<endl;
+ cout <<"         ##                                                                           ##  "<<endl;
+ cout <<"         ##                                                                           ##  "<<endl;
+ cout <<"         ##                                                                           ##  "<<endl;
+ cout <<"         ##                                                                           ##  "<<endl;
+ cout <<"         ##                  LIBRARY     MANAGMENT      SYSTEM                        ##  "<<endl;
+ cout <<"         ##                                                                           ##  "<<endl;
+ cout <<"         ##                                                                           ##  "<<endl;
+ cout <<"         ##                                                                           ##  "<<endl;
+ cout <<"         ##                                                                           ##  "<<endl;
+ cout <<"         ##                                                                           ##  "<<endl;
+ cout <<"         ##                                                                           ##  "<<endl;
+ cout <<"         ##                                                                           ##  "<<endl;
+ cout <<"         ##                                                                           ##  "<<endl;
+ cout <<"         ###############################################################################  "<<endl;
+}
